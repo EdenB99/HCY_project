c2ee3c9580afabbdb1df8c8f735c26ceee5368ae
EdenB99 <rkdguswns99@gmail.com>
2025-03-12 16:12:32 +0900

    03_12 강현준
    캐릭터 AC 및 모션 삽입 시작,
    캐릭터 모델 실 적용

--- Assets/Script/Enemy/WaveManager.cs
+++ Assets/Script/Enemy/WaveManager.cs
@@ -26,43 +26,5 @@ public class WaveManager : MonoBehaviour
 
     private void Start()
     {
-        StartNextWave();
     }
-
-    /// <summary>
-    /// 다음 웨이브를 시작
-    /// </summary>
-    public void StartNextWave()
-    {
-        if (currentWaveIndex >= waveConfigs.waves.Count)
-        {
-            Debug.Log("모든 웨이브 완료!");
-            return;
-        }
-        EnemyWaveData currentWaveData = waveConfigs.waves[currentWaveIndex];
-        StartCoroutine(HandleWave(currentWaveData));
-        currentWaveIndex++;
-    }
-    /// <summary>
-    /// 웨이브 실행 및 적 생성
-    /// </summary>
-    private IEnumerator HandleWave(EnemyWaveData waveData)
-    {
-        isWaveActive = true;
-        Debug.Log($"[WaveType: {waveData.waveType}] 웨이브 진행 중");
-        foreach (var enemyGroup in waveData.enemyGroups)
-        {
-            for (int i = 0; i < enemyGroup.count; i++)
-            {
-                EnemySpawner.Instance.SpawnEnemy(enemyGroup.enemy, movementPath);
-                yield return new WaitForSeconds(1f); // 몬스터 생성 간격
-            }
-        }
-        isWaveActive = false;
-        Debug.Log("웨이브 종료!");
-
-        yield return new WaitForSeconds(3f); // 웨이브 종료 후 대기 시간
-        StartNextWave(); // 다음 웨이브 진행
-    }
-    
 }
